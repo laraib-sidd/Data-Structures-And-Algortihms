08a7af0d9949fb4125da883875ddb3fa1fd90128
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-03 14:58:56 +0530

    complete

--- Array/string_reverse.py
+++ Array/string_reverse.py
@@ -5,8 +5,14 @@ Input : "Hi how are you?"
 Output : "?uoy era woh iH"
 '''
 def reverse(string):
+    """
+    Function to reverse string
+    """
     string = list(string)
     string = string[::-1]
     string = "".join(string)
     return string
 
+
+word = reverse("This function Reverses string")
+print(word)
