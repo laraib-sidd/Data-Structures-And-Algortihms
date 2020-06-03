44e45f2ca374fc916e5b80d1594b0eace8ada33b
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-03 18:41:59 +0530

    final patch

--- Array/merge_sort.py
+++ Array/merge_sort.py
@@ -1,4 +1,20 @@
+# Shortcut way
+#  def mergesortedarr(a,b):
+#  x=a+b
+#  x.sort()
+#  return x
+
+# a=[1,2,3,4]
+# b=[3,7,9,12]
+# qw=mergesortedarr(a,b)
+# print(qw)
+
+#In interview we must solve only like this
+
 def mergesortarray(arr1,arr2):
+    '''
+    Function to implement merge
+    '''
 
     if len(arr1)==0 or len(arr2)==0:
         return arr1 + arr2
@@ -7,13 +23,20 @@ def mergesortarray(arr1,arr2):
     i = 0
     j = 0
     
-    while i<len(arr2) or j < len(arr2):
-        if arr1[i]<=arr2[j]:
+    while i < len(arr1) and j < len(arr2):
+        print(arr1[i],arr2[j])
+        if arr1[i] <= arr2[j]:
             mereged_array.append(arr1[i])
             i+=1
         
-        elif arr1[i] >= arr2[j]:
+        elif arr1[i] > arr2[j]:
             mereged_array.append(arr2[j])
             j+=1      
 
-    return mereged_array
+    return mereged_array+arr1[i:]+arr2[j:]
+
+# Example
+a=[1,3,4,6,20]
+b=[2,3,4,5,6,9,11,76]
+arr=mergesortarray(a,b)
+print(arr)
