a22f54d6031831d4a19d34ff0f0984d40fc86280
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-03 14:31:10 +0530

    complete array with examples

--- Array/my_array.py
+++ Array/my_array.py
@@ -2,6 +2,9 @@ class MyArray:
     def __init__(self):
         self.length = 0
         self.data = {}
+
+    def __str__(self):
+        return str(self.__dict__)
     
     def get(self,index):
         return self.data[index]
@@ -25,3 +28,15 @@ class MyArray:
         del self.data[self.length-1]
         self.length-=1
         return item
+
+# Examples 
+arr=MyArray()
+arr.push(3)
+arr.push('hi')
+arr.push(34)
+arr.push(20)
+arr.push('hey')
+arr.push('welcome')
+arr.delete(3)
+print(arr)
+  
