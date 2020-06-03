9620071c9d1c835e797b701c8f1d9582d058ad22
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-03 18:14:46 +0530

    hel

--- Array/merge_sort.py
+++ Array/merge_sort.py
@@ -1,6 +1,9 @@
 def mergesortarray(arr1,arr2):
     if len(arr1)==0 or len(arr2)==0:
         return arr1 + arr2
-    mereged_array = None
-    
+    mereged_array = []
+    i = 0
+    j = 0
+    while i<len(arr2) or j < len(arr2):
+        pass
     return mereged_array
