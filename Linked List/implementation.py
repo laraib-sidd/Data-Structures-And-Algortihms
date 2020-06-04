1ce4e4108d999a4a0cc2655e8391fec964f23a3b
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-04 20:56:05 +0530

    formatting update

--- Linked List/implementation.py
+++ Linked List/implementation.py
@@ -3,33 +3,30 @@ Linked List Implemetation
 15 --> 6 --> 8
 '''
 
+
 class Node():
 
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
-    
+
 
 class LinkedList():
 
     def __init__(self):
         self.head = None
         self.tail = None
-    
 
-    def  append(self,data):
+    def append(self, data):
         new_node = Node(data)
-        if self.head == None:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
             self.length = 1
-        
         else:
             self.tail.next = new_node
             self.tail = new_node
-            self.length +=1
-    
+            self.length += 1
 
-    def prepend(self,data):
+    def prepend(self, data):
         new_node = Node(data)
-        
