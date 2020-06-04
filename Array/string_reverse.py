a438dd974553be7594eca87cc351580c48824b2a
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-04 14:16:53 +0530

    formatting

--- Array/string_reverse.py
+++ Array/string_reverse.py
@@ -4,18 +4,21 @@ Example:
 Input : "Hi how are you?"
 Output : "?uoy era woh iH"
 '''
+
+
 def reverse(string):
     """
     Function to reverse string
-    """ 
+    """
     try:
-        if string or len(string)>2:
+        if string or len(string) > 2:
             string = list(string)
             string = string[::-1]
             string = "".join(string)
             return string
-    except:
+    except KeyboardInterrupt:
         print("Check Your Input")
+
 
 word = reverse("This function Reverses string")
 print(word)
