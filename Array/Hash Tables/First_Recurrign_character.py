05e0359258ee96cc3877b77edc00875848cc94a1
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-04 12:44:36 +0530

    changes

--- Array/Hash Tables/First_Recurrign_character.py
+++ Array/Hash Tables/First_Recurrign_character.py
@@ -12,13 +12,3 @@ It should return undefined """
  [2,5,5,2,3,5,1,2,4]
 return 5 because the pairs are before 2,2 """
 
-
-def func(mylist):
-    count_dict = {}
-    for i in mylist:
-        if i not in count_dict.values():
-            count_dict[i] = 1
-        else:
-            count_dict[i] = count_dict[i] + 1
-    return count_dict
-    
