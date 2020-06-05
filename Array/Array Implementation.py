c783430df07148ea49d57c0b13f171cf4877840e
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-05 18:53:14 +0530

    new

--- Array/Array Implementation.py
+++ Array/Array Implementation.py
@@ -1,11 +1,12 @@
 class MyArray:
+
     def __init__(self):
         self.length = 0
         self.data = {}
 
     def __str__(self):
         return str(self.__dict__)
-    
+
     def get(self,index):
         return self.data[index]
 
