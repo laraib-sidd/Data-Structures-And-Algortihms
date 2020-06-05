64f6e8ca5363acc3feb222c8c3e890096589d2c0
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-05 18:54:29 +0530

    formati

--- Array/Array Implementation.py
+++ Array/Array Implementation.py
@@ -7,31 +7,32 @@ class MyArray:
     def __str__(self):
         return str(self.__dict__)
 
-    def get(self,index):
+    def get(self, index):
         return self.data[index]
 
-    def push(self,item):
+    def push(self, item):
         self.data[self.length] = item
-        self.length+=1  
+        self.length += 1
         return self.length
 
     def pop(self):
-        last = self.data[self.length - 1] 
+        last = self.data[self.length - 1]
         del self.data[self.length-1]
-        self.length-=1
+        self.length -= 1
         return last
-    
-    def delete(self,index):   
+
+    def delete(self, index):
         item = self.data[index]
-        for i in range(index,self.length-1):
+        for i in range(index, self.length-1):
             self.data[i] = self.data[i+1]
-    
+
         del self.data[self.length-1]
-        self.length-=1
+        self.length -= 1
         return item
 
-# Examples 
-arr=MyArray()
+
+# Examples
+arr = MyArray()
 arr.push(3)
 arr.push('hi')
 arr.push(34)
@@ -40,5 +41,3 @@ arr.push('hey')
 arr.push('welcome')
 arr.delete(3)
 print(arr)
-
-  
