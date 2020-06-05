0e0bad5658f5bd024f2a7503650d58b77b822037
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-05 19:40:07 +0530

    final

--- Stacks And Queues/Stacks using Linked List.py
+++ Stacks And Queues/Stacks using Linked List.py
@@ -28,18 +28,29 @@ class Stack:
             self.bottom = new_node
             self.top = new_node
             self.length = 1
-        else:
-            self.top.next = new_node
-            self.top = new_node
-            self.length += 1
+            return
+        self.top.next = new_node
+        self.top = new_node
+        self.length += 1
 
     def pop(self):
-        i = 1 
+        i = 1
         curr_node = self.bottom
-        while 
+        while i != self.length-1:
+            curr_node = curr_node.next
+            i += 1
+        popped_value = curr_node.next
+        curr_node.next = None
+        self.top = curr_node
+        self.length -= 1
+        return popped_value.data
 
     def printt(self):
-        pass
+        temp = self.bottom
+        while temp is not None:
+            print(temp.data, end=' -> ')
+            temp = temp.next
+        print()
 
 
 # Example
@@ -47,3 +58,5 @@ st = Stack()
 st.push('Google')
 st.push('9Anime')
 st.push('Legend of the galactic heroes')
+st.pop()
+st.printt()
