c9745cc29624fbd25421cda1bbf9f2730c8701f9
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-05 20:20:54 +0530

    sa

--- Array/String reverse.py
+++ Array/String reverse.py
@@ -19,6 +19,7 @@ def reverse(string):
     except KeyboardInterrupt:
         print("Check Your Input")
 
+
 if __name__ == "__main__":
     word = reverse("This function Reverses string")
     print(word)
