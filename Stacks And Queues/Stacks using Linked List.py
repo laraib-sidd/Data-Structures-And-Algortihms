3850ffcfce596d51b2ac14e6b7edc3c6676b8678
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-05 19:01:04 +0530

    comit

--- Stacks And Queues/Stacks using Linked List.py
+++ Stacks And Queues/Stacks using Linked List.py
@@ -1,4 +1,19 @@
 class Node:
+
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
+class Stack:
+
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.length = 0
+
+    def peek(self):
+        pass
+
+    def push(self, data):
+        pass
