535f2e79e279da80aa66028057dd9a5345a965f4
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-05 20:35:39 +0530

    ds

--- Stacks And Queues/Stack using Arrays.py
+++ Stacks And Queues/Stack using Arrays.py
@@ -4,11 +4,29 @@ class Stack:
         self.length = 0
         self.arr = []
 
+    def __str__(self):
+        return str(self.__dict__())
+
+    def peek(self):
+        
+
+
     def isEmpty(self):
         return len(self.arr) == 0
+
+    def push(self, data):
+        return self.arr.append(data)
+
+    def pop(self):
+        return self.arr.pop()
 
 
 # Driver Code
 if __name__ == "__main__":
     st = Stack()
+    st.push('Google')
+    st.push('9Anime')
+    st.push('Legend of the galactic heroes')
+    st.pop()
     print(st.isEmpty())
+    print(st.arr)
