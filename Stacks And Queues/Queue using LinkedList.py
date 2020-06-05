99d76a4289ba3cea96645d71ca1390784f3042d4
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-05 20:54:45 +0530

    ds

--- Stacks And Queues/Queue using LinkedList.py
+++ Stacks And Queues/Queue using LinkedList.py
@@ -4,7 +4,10 @@ class Node:
         self.data = data
         self.next = None
 
+
 class Queue:
 
     def __init__(self):
-    
+        self.first = None
+        self.last = None
+        self.lenght = 0
