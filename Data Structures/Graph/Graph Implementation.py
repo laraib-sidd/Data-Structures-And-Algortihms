0264c93135d55cdeaa30de820a602689aab1d40e
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-09 18:49:16 +0530

    example

--- Data Structures/Graph/Graph Implementation.py
+++ Data Structures/Graph/Graph Implementation.py
@@ -9,11 +9,12 @@ class Graph:
         return str(self.__dict__)
 
     def addvertex(self, node):
-        self.adjacentlist['node'] = []
+        self.adjacentlist[node] = []
         self.numberofnodes += 1
 
     def addedge(self, node1, node2):
-        pass
+        self.adjacentlist[node1].append(node2)
+        self.adjacentlist[node2].append(node1)
 
     def showconnection(self):
         pass
@@ -21,4 +22,20 @@ class Graph:
 
 # Driver code
 if __name__ == "__main__":
-    pass
+    myGraph = Graph()
+    myGraph.addVertex('0')
+    myGraph.addVertex('1')
+    myGraph.addVertex('2')
+    myGraph.addVertex('3')
+    myGraph.addVertex('4')
+    myGraph.addVertex('5')
+    myGraph.addVertex('6')
+    myGraph.addEdge('3', '1')
+    myGraph.addEdge('3', '4')
+    myGraph.addEdge('4', '2')
+    myGraph.addEdge('4', '5')
+    myGraph.addEdge('1', '2')
+    myGraph.addEdge('1', '0')
+    myGraph.addEdge('0', '2')
+    myGraph.addEdge('6', '5')
+    print(myGraph)
