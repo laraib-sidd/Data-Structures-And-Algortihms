71d05d8e6d76d12857f0c91fa09f298dd01ea1fa
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-09 23:24:40 +0530

    recursion

--- Algorithms/Sorting/file.py
+++ Algorithms/Sorting/file.py
@@ -6,9 +6,22 @@ One should use recursive , the other should just use a for loop.
 
 # Recursive
 def fact_recursive(number):
-    pass
+    if number == 1:
+        return 1
+    return number * fact_iterative(number - 1)
 
 
 # Iterative
 def fact_iterative(number):
-    pass
+    if number == 1:
+        return 1
+    fact = 1
+    for i in range(1, number + 1):
+        fact = fact * i
+    return fact
+
+
+# Driver code
+if __name__ == "__main__":
+    print(fact_iterative(7))
+    print(fact_recursive(8))
