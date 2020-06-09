a174a7d69ad877b17b6fd95963ede729e27073b3
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-09 15:16:57 +0530

    sa

--- Data Structures/Trees/Binary Search Tree.py
+++ Data Structures/Trees/Binary Search Tree.py
@@ -116,7 +116,8 @@ class BinarySearchTree:
                 if curr_node.right is None:
                     if parent_node is None:
                         self.root = curr_node.left
-                    
+
+
                 elif curr_node.left is None:
                     curr_node = curr_node.right
                     return True
