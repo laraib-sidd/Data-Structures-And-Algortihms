0f28008fc3fb70a649b93327648cd750d26d9b0d
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-09 17:38:23 +0530

    sa

--- Data Structures/Trees/heapq in python.py
+++ Data Structures/Trees/heapq in python.py
@@ -11,5 +11,13 @@ x = [2, 32, 213, 12, 321, 312]
 heapq.heapify(x)
 print(x)
 
+# To push elements in the heap.
 heapq.heappush(x, 0)
+print(x)
+
+print(heapq.heappop(x))
+print(x)
+
+# Used to pop and push the element in same time
+print (heapq.heappushpop(x, 5)) 
 print(x)
