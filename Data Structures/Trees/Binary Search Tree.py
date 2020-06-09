0ccd431b9fd3d2e1e7b351d76f4e304506b67459
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-09 14:51:11 +0530

    s

--- Data Structures/Trees/Binary Search Tree.py
+++ Data Structures/Trees/Binary Search Tree.py
@@ -95,7 +95,8 @@ class BinarySearchTree:
                 curr_node = curr_node.left
             elif data > curr_node.data:
                 curr_node = curr_node.right
-
+    def remove(self, data):
+        pass
     def print_tree(self):
         if self.root is not None:
             self.printt(self.root)
