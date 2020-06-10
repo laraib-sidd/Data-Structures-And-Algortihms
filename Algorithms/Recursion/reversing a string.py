5e29b66e33a2f01cdcf33d99dcb4d60e0427fb24
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-10 23:19:00 +0530

    update

--- Algorithms/Recursion/reversing a string.py
+++ Algorithms/Recursion/reversing a string.py
@@ -4,19 +4,21 @@ and then recursion!'''
 
 # Iterative
 def iter_reverseString(str):
-    try:
-        if str or str > 2:
-            str = list(str)
-            str = str[::-1]
-            str = "".join(str)
-            print(str)
-            return True
-    except:
+
+    if str or str > 2:
+        str = list(str)
+        str = str[::-1]
+        str = "".join(str)
+        print(str)
+        return True
+    else:
         print("Incorrect Input")
 
 
+# Recursive
 def recur_reverseStrign(str):
-    
+    pass
+
 
 if __name__ == "__main__":
     iter_reverseString('My iterative example')
