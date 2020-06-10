8255bcb7be2ee2f7c08d2c7ed7149d2db10079db
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-10 13:22:25 +0530

    update

--- Algorithms/Recursion/fibonacci.py
+++ Algorithms/Recursion/fibonacci.py
@@ -13,7 +13,7 @@ For example: fibonacciRecursive(6) should return 8
 def fibo_recursive(num):
     if num < 2:
         return num
-    return fibo_iterative(num - 1) + fibo_recursive(num - 2)
+    return fibo_recursive(num - 2) + fibo_recursive(num - 1)
 
 
 def fibo_iterative(num):
@@ -23,4 +23,4 @@ def fibo_iterative(num):
 # Driver code
 if __name__ == "__main__":
     print(f"Recursion : {fibo_recursive(8)}")
-    print(f"Iterative : {fibo_iterative(6)}")
+    # print(f"Iterative : {fibo_iterative(6)}")
