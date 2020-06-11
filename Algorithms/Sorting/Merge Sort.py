5b72e17dc0106402f3889c18c8d2129eb9a72989
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-11 17:36:15 +0530

    updat

---
+++ Algorithms/Sorting/Merge Sort.py
@@ -0,0 +1,14 @@
+'''
+Merge Sort: It uses Divide and Conquer and recursion
+
+Time Complexity : O(n log(n))
+Space Complexity : O(n)
+'''
+
+
+def mergeSort(arr):
+    pass
+
+
+if __name__ == "__main__":
+    pass
