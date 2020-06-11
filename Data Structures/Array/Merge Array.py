90283aead7abeb61e67de188394226c7feaed7a1
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-11 17:39:06 +0530

    update

--- Data Structures/Array/Merge Array.py
+++ Data Structures/Array/Merge Array.py
@@ -15,10 +15,8 @@ def mergesortarray(arr1, arr2):
     '''
     Function to implement merge
     '''
-
     if len(arr1) == 0 or len(arr2) == 0:
         return arr1 + arr2
-
     mereged_array = []
     i = 0
     j = 0
@@ -28,11 +26,9 @@ def mergesortarray(arr1, arr2):
         if arr1[i] <= arr2[j]:
             mereged_array.append(arr1[i])
             i += 1
-
         elif arr1[i] > arr2[j]:
             mereged_array.append(arr2[j])
             j += 1
-
     return mereged_array+arr1[i:]+arr2[j:]
 
 
