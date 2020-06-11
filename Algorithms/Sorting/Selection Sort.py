be3bf8ab61ce7d01ec4b69af4144f61701b37ff5
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-11 16:36:53 +0530

    update

--- Algorithms/Sorting/Selection Sort.py
+++ Algorithms/Sorting/Selection Sort.py
@@ -2,14 +2,16 @@
 Slelection Sort:
 The algorithm goes around the list picking out the smallest
 item in the list and putting it at the top.
-
+Time Complexity: O(n^2)
+Space Complexity: O(1)
 '''
 
 
 def slectionSort(arr):
-    pass
+    i
 
 
 if __name__ == "__main__":
     numbers = [32, 43, 32, 1, 23, 21, 465, 987]
     print(slectionSort(numbers))
+    
