2bef76d4858248578f38ed9e30686398e0d6b59b
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-11 12:53:05 +0530

    update

--- Algorithms/Sorting/Bubble Sort.py
+++ Algorithms/Sorting/Bubble Sort.py
@@ -1,3 +1,7 @@
 '''
-Bubble Sort
+Bubble Sort:
+The algorithm goes along the values in the list one by one,
+if the neighbour is larger, it switches the position.
+It keeps on going untill no switching is found.
+
 '''
