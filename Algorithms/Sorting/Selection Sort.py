aad15d57d891ad5115b0723ec841b86367fd3974
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-11 16:15:20 +0530

    update

--- Algorithms/Sorting/Selection Sort.py
+++ Algorithms/Sorting/Selection Sort.py
@@ -4,3 +4,11 @@ The algorithm goes around the list picking out the smallest
 item in the list and putting it at the top.
 
 '''
+
+
+def slectionSort(arr):
+    pass
+
+
+if __name__ == "__main__":
+    pass
