31bb43ce72afdb313fe86ffa02fc3cedfee667d2
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-11 16:43:30 +0530

    update

--- Algorithms/Sorting/Selection Sort.py
+++ Algorithms/Sorting/Selection Sort.py
@@ -10,17 +10,18 @@ Space Complexity: O(1)
 def slectionSort(arr):
     i = 0
     while i < len(arr):
-        minn = arr[i]
+        min = arr[i]
         index = i
-        for j in range(i + 1, len(arr)):
-            if arr[j] < minn:
-                index = j
-                minn = arr[j]
-            arr[i], arr[index] = arr[index], arr[i]
-            i += 1
+        for j in range(i+1, len(arr)):
+        if arr[j] < min:
+            index = j
+            min = arr[j]
+        arr[i] , arr[index] = arr[index] , arr[i]
+        i += 1
+    
     return arr
-
 
 if __name__ == "__main__":
     num = [32, 43, 32, 1, 23, 21, 465, 987]
     print(slectionSort(num))
+    print('fds')
