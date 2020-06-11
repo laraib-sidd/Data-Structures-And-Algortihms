1c8e4ac8ba5278f17842bf8522da9b375064510b
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-11 16:19:25 +0530

    update

--- Algorithms/Sorting/Selection Sort.py
+++ Algorithms/Sorting/Selection Sort.py
@@ -7,8 +7,15 @@ item in the list and putting it at the top.
 
 
 def slectionSort(arr):
-    pass
+    smallest = None
+    num = []
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)):
+            if arr[j] < arr[i]:
+                smallest = arr[j]
+        num.append(smallest)
+    return num
 
 
 if __name__ == "__main__":
-    pass
+    
