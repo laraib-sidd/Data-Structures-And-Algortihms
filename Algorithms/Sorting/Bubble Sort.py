99c613d119b255905d984a74a699294dfcfe54dc
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-11 12:56:41 +0530

    update

--- Algorithms/Sorting/Bubble Sort.py
+++ Algorithms/Sorting/Bubble Sort.py
@@ -1,7 +1,14 @@
 '''
 Bubble Sort:
 The algorithm goes along the values in the list one by one,
-if the neighbour is larger, it switches the position.
+if the neighbour is smaller, it switches the position.
 It keeps on going untill no switching is found.
+
+Example : [3,1,4]
+* First it will compare '3' and '1' as '1' is
+smaller it switeches, its positions with '3'.
+* Than it checks for '3' and '4', as '4' is already bigger,
+no switching.
+* As there is no more switching, so the list is sorted.
 
 '''
