030291ec0d8e09bef9a22dee932fc8c11b5d9184
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-14 22:22:09 +0530

    update

--- Algorithms/Sorting/Quick Sort.py
+++ Algorithms/Sorting/Quick Sort.py
@@ -3,4 +3,7 @@ Quick Sort:
 We pick a random pivot point, compare the values if the value
 is smaller we put it too the left of it,
 if bigger we put it too the righ of it.
+
+Time Complexity : O(n log(n))
+Space Complexity : O(n log(n))
 """
