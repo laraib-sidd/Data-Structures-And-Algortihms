fbf514761c517c15c7a84a2aa6588a5d375545b8
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-15 10:38:25 +0530

    update

--- Algorithms/Searching/Exercise.py
+++ Algorithms/Searching/Exercise.py
@@ -1,23 +1,29 @@
 """
 If you know a solution is not far from the root of the tree:
+Breadth First Search
 """
 
 """
 If the tree is very deep and solutions are rare:
+* Depth First Search
 """
 
 """
 If the tree is very wide:
+* Breadth First Search
 """
 
 """
 If solutions are frequent but located deep in the tree:
+* Depth First Search
 """
 
 """
 Determining whether a path exists between two nodes:
+* Depth First Search
 """
 
 """
 Finding the shortest path:
+* Breadth First Search
 """
