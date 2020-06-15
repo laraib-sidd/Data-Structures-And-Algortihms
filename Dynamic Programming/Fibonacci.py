c72acd0f1fd9be897aa9f720afb35fc80ffb5aaa
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-15 15:08:57 +0530

    update

--- Dynamic Programming/Fibonacci.py
+++ Dynamic Programming/Fibonacci.py
@@ -1,3 +1,14 @@
 """
 Creating the fibonacci program using Dynamic Programming
 """
+from functools import lru_cache
+
+
+@lru_cache(maxsize=1000)
+def fib_dp(num):
+    if num < 2:
+        return num
+    else:
+        return fib_dp(num - 1) + fib_dp(num - 2)
+
+    
