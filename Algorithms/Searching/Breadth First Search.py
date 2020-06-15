edf6d44bb7f0f145a650a1e48f0e7a3395fbbd04
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-15 10:25:37 +0530

    update

--- Algorithms/Searching/Breadth First Search.py
+++ Algorithms/Searching/Breadth First Search.py
@@ -11,3 +11,17 @@ class Node:
 class BinaryFirstSearch:
     def __init__(self):
         pass
+
+    def insert(self, value):
+        pass
+
+    def lookup(self, value):
+        pass
+
+    def remove(self, value):
+        pass
+
+
+# Driver Code
+if __name__ == "__main__":
+    pass
