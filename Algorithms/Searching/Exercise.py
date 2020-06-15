625d3b6286bedacf46524505c8d9c3751b667733
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-15 10:36:48 +0530

    update

---
+++ Algorithms/Searching/Exercise.py
@@ -0,0 +1,23 @@
+"""
+If you know a solution is not far from the root of the tree:
+"""
+
+"""
+If the tree is very deep and solutions are rare:
+"""
+
+"""
+If the tree is very wide:
+"""
+
+"""
+If solutions are frequent but located deep in the tree:
+"""
+
+"""
+Determining whether a path exists between two nodes:
+"""
+
+"""
+Finding the shortest path:
+"""
